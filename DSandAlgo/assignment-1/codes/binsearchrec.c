c2f5b1b4b1f792b91e039b5322b7e3ad9540e0e1
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-04 20:55:46 +0530

    DS: assignment1: reformat using vscode format
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- DSandAlgo/assignment-1/codes/binsearchrec.c
+++ DSandAlgo/assignment-1/codes/binsearchrec.c
@@ -1,19 +1,19 @@
-//C programming code for binary search using recursion 
+//C programming code for binary search using recursion
 #include <stdio.h>
- 
+
 int RecursiveBinarySearching(int arr[], int low, int high, int element)
 {
       int middle;
-      if(low > high)
+      if (low > high)
       {
             return -1;
       }
       middle = (low + high) / 2;
-      if(element > arr[middle])
+      if (element > arr[middle])
       {
             RecursiveBinarySearching(arr, middle + 1, high, element);
       }
-      else if(element < arr[middle])
+      else if (element < arr[middle])
       {
             RecursiveBinarySearching(arr, low, middle - 1, element);
       }
@@ -22,21 +22,21 @@ int RecursiveBinarySearching(int arr[], int low, int high, int element)
             return middle;
       }
 }
- 
+
 int main()
 {
       int count, element, limit, arr[50], position;
       printf("\nEnter the Limit of Elements in Array:\t");
       scanf("%d", &limit);
       printf("\nEnter %d Elements in Array: \n", limit);
-      for(count = 0; count < limit; count++)
+      for (count = 0; count < limit; count++)
       {
             scanf("%d", &arr[count]);
       }
       printf("\nEnter Element To Search:\t");
       scanf("%d", &element);
       position = RecursiveBinarySearching(arr, 0, limit - 1, element);
-      if(position == -1)
+      if (position == -1)
       {
             printf("\nElement %d Not Found\n", element);
       }
