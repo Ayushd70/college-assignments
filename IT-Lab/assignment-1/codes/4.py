b718e771ca120d2476ac103a8fce5a937ed86158
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-11 20:05:59 +0530

    IT:assignment-1: add question 4 py file
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-1/codes/4.py
@@ -0,0 +1,8 @@
+print("Enter principal amount:")
+pa = float(input())
+print("Enter annual interest")
+ai = float(input())
+print("Enter number of years")
+years = int(input())
+
+print("Total:", ((pa-ai)*years))
