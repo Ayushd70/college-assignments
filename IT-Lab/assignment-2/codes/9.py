fe7daee065eb9c938ac35acd27a8742d3ecab9b3
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-15 12:37:14 +0530

    IT:assignment-2: add question 9 py file
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-2/codes/9.py
@@ -0,0 +1,20 @@
+def ArmStrong(n):
+  l = len(str(n))
+  sum = 0
+  temp = n
+  
+  while(temp > 0):
+    d = temp % 10
+    sum += d ** l
+    temp //= 10
+
+  if(n == sum):
+    return "Armstrong number"
+  else:
+    return "Not an Armstrong number"
+
+num = int(input())
+
+print(ArmStrong(num))
+print(ArmStrong(153))
+print(ArmStrong(67))
