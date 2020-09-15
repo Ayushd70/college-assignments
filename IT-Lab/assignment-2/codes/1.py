35d59fcc853c90593944e797ef098612e10f500f
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-15 12:27:10 +0530

    IT:assignment-2: add question 1 py file
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-2/codes/1.py
@@ -0,0 +1,8 @@
+x = input("Enter 1st number:")
+y = input("Enter 2nd number:")
+if x == y:
+    print("Equal")
+elif x > y:
+    print("Number 2 < Number 1")
+else:
+    print("Number 2 > Number 1")
