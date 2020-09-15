10c4be0e8d9ffdfbb5b2801e4fce87e731af33a5
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-15 12:36:25 +0530

    IT:assignment-2: add question 8 py file
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-2/codes/8.py
@@ -0,0 +1,8 @@
+try:
+  print("Enter Hours: ")
+  hrs = int(input())
+  print("Enter Rate: ")
+  rate = int(input())
+  print("Pay:",hrs*rate)
+except ValueError:
+  print("Error, please enter numeric input")
