4aa2f72521078abaecc6cb2dca2192093d53fbb9
Ayush Dubey <ayushdubey70@gmail.com>
2020-11-06 19:25:24 +0530

    IT: assignment-4: add question 1 py file
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-4/codes/1.py
@@ -0,0 +1,11 @@
+def check(s1, s2):
+  if s1 == s2[::-1]:
+    print("Given string is Palindrome")
+  else:
+    print("Given string is not Palindrome.")
+
+s1 = input()
+s2 = input()
+
+if len(s1) == len(s2):
+  check(s1, s2)
