a359b004aa04e3f2b2168485e6d246091c6eb68d
Ayush Dubey <ayushdubey70@gmail.com>
2020-11-12 13:24:27 +0530

    IT: format assignment 4 using black
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- IT-Lab/assignment-4/codes/2.py
+++ IT-Lab/assignment-4/codes/2.py
@@ -1,8 +1,9 @@
 def rev_sentence(sentence):
 
-    words = sentence.split(' ')
-    reverse_sentence = ' '.join(reversed(words))
+    words = sentence.split(" ")
+    reverse_sentence = " ".join(reversed(words))
     return reverse_sentence
+
 
 string = input("Enter a sentence: ")
 print(rev_sentence(string))
--- IT-Lab/assignment-4/codes/3.py
+++ IT-Lab/assignment-4/codes/3.py
@@ -1,7 +1,9 @@
 def init(str):
-    a=str[0].upper()
+    a = str[0].upper()
     for i in range(1, len(str)):
-        if (str[i]==' '):
-            print(a, str[i+1].upper())
+        if str[i] == " ":
+            print(a, str[i + 1].upper())
+
+
 str = "Ayush Dubey"
 init(str)
