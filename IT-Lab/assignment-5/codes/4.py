8c4c42e3c543f2151ca7a9bec0f2f4e4dd8bbf27
Ayush Dubey <ayushdubey70@gmail.com>
2020-11-12 12:51:25 +0530

    IT: assignment-5: an equivalent of switch case be implemented in Python.
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-5/codes/4.py
@@ -0,0 +1,11 @@
+def numbers_to_strings(argument): 
+    switcher = { 
+        0: "zero", 
+        1: "one", 
+        2: "two", 
+    } 
+    return switcher.get(argument, "nothing") 
+  
+if __name__ == "__main__": 
+    k = int(input("Enter the case: "))
+    print (numbers_to_strings(k))
