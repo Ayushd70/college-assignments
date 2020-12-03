929040f408eaf0fbda421aaf9f409aef697edd9b
Ayush Dubey <ayushdubey70@gmail.com>
2020-12-03 15:27:05 +0530

    IT: assignment-8: add question 6 py file
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-8/codes/6.py
@@ -0,0 +1,13 @@
+data = {"apple": 5600.00, "orange": 3500.00, "banana": 5000.00}
+
+data1 = {}
+
+if data:
+    print("dictionary is not empty")
+else:
+    print("dictionary is empty")
+
+if data1:
+    print("dictionary is not empty")
+else:
+    print("dictionary is empty")
