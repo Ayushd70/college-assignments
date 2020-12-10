c5854d87e9cec0303e242ec3864f82ff538d4ce4
Ayush Dubey <ayushdubey70@gmail.com>
2020-12-10 15:39:06 +0530

    IT: assignment-9: add question 6 py file
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-9/6.py
@@ -0,0 +1,8 @@
+employee = {
+    "raman": {"age": "27", "sex": "Male", "salary": "10000"},
+    "ayush": {"age": "22", "sex": "Male", "salary": "20000"},
+    "gal": {"age": "22", "sex": "Female", "salary": "50000"},
+}
+for k in employee:
+    print("name=", k, "\n")
+    print("info=", employee[k], "\n")
