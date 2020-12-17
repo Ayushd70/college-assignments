9d006249991b0a8375c92d5ba0f9b7f8aee4f319
Ayush Dubey <ayushdubey70@gmail.com>
2020-12-17 20:20:33 +0530

    IT: assignment-9: add question 2 py file
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-10/codes/2.py
@@ -0,0 +1,15 @@
+def printPascal(n: int):
+    arr = [[0 for x in range(n)] for y in range(n)]
+    for line in range(0, n):
+        for i in range(0, line + 1):
+            if i is 0 or i is line:
+                arr[line][i] = 1
+                print(arr[line][i], end=" ")
+            else:
+                arr[line][i] = arr[line - 1][i - 1] + arr[line - 1][i]
+                print(arr[line][i], end=" ")
+        print("\n", end="")
+
+
+n = int(input("Enter the number of rows="))
+printPascal(n)
