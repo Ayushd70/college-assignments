678b83d4a0b19d65760b62bd0a9833ca6eba4e0d
Ayush Dubey <ayushdubey70@gmail.com>
2021-01-31 21:04:54 +0530

    IT: assignment-14: add code for problem 2
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ IT-Lab/assignment-14/codes/2.py
@@ -0,0 +1,8 @@
+def rev_sentence(sentence):
+    words = sentence.split(" ")
+    reverse_sentence = " ".join(reversed(words))
+    return reverse_sentence
+
+
+string = input("Enter a sentence: ")
+print(rev_sentence(string))
