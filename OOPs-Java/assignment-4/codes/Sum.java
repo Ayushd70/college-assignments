85ef0a8e8319718b305f0131a66ded15adb566e8
Ayush Dubey <ayushdubey70@gmail.com>
2021-10-05 15:24:27 +0530

    OOPs: assignment-4: Create class sum and demonstrate constructor overloading
    * by adding two,three parameters
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- OOPs-Java/assignment-4/codes/Sum.java
+++ OOPs-Java/assignment-4/codes/Sum.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class Sum {
+    int sumn;
+
+    public Sum() {
+        System.out.println("Bucket is empty");
+    }
+
+    public Sum(int n1, int n2) {
+        this.sumn = n1 + n2;
+    }
+
+    public Sum(int n1, int n2, int n3) {
+        this.sumn = n1 + n2 + n3;
+    }
+
+    public int getSumn() {
+        return this.sumn;
+    }
+
+    public static void main(String[] args) {
+        int n1, n2, n3;
+        new Sum(); //Constructor with no arg
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        System.out.println(new Sum(n1, n2).getSumn()); // two args
+        System.out.println("Enter three nums");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        n3 = sc.nextInt();
+        System.out.println(new Sum(n1, n2, n3).getSumn()); //three args
+    }
 }
