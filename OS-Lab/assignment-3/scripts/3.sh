69fcd244a4f3d643d3a8cfe2ae86934160e2394f
Ayush Dubey <ayushdubey70@gmail.com>
2021-10-08 13:37:09 +0530

    OS: assignment-3: update program 3
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- OS-Lab/assignment-3/scripts/3.sh
+++ OS-Lab/assignment-3/scripts/3.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-read -r n
-x=0
-y=1
-i=2
-echo "Fibonacci Series up to $n terms :"
-echo "$x"
-echo "$y"
-while [ $i -lt "$n" ]; do
-    i=$((i + 1))
-    z=$((x + y))
-    echo "$z"
-    x=$y
-    y=$z
-done
+read -r -p "Enter file name: " f
+read -r -p "Enter line start: " l
+read -r -p "Enter line end: " e
+more +"$l" -"$e" "$f"
