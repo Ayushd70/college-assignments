1846391987e7db76a7d75aea208421c9bb1c73da
Ayush Dubey <ayushdubey70@gmail.com>
2021-10-08 13:20:19 +0530

    OS: assignment-3: shell script to count number of words, characters, white spaces and special symbols
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ OS-Lab/assignment-3/scripts/15.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+read -r -p "Enter string: " str
+c=$(echo $str | wc -c)
+c=$(expr $c - 1)
+w=$(echo $str | wc -w)
+w=$(expr $w)
+wsp=0
+a=0
+sp=\` \`
+n=1
+while [ $n -le $c ]; do
+    ch=$(echo $str | cut -c $n)
+    if [ $ch == $sp ]; then
+        wsp=$(expr $wsp + 1)
+    else
+        case $ch in
+        [a-z] | [A-Z]) a=$(expr $a + 1) ;;
+        esac
+    fi
+    n=$(expr $n + 1)
+done
+
+spec=$(expr $c - $wsp - $a)
+echo chars=$c
+echo spaces=$wsp
+echo spec symbols=$spec
+echo words=$w
