586d64868b528410ecb3a6e017767d40c2483787
Ayush Dubey <ayushdubey70@gmail.com>
2021-11-09 19:46:56 +0530

    OOPs: assignemnt-7: outputs: output for program 1
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- OOPs-Java/assignment-7/codes/Student.java
+++ OOPs-Java/assignment-7/codes/Student.java
@@ -1,28 +1,24 @@
-class student {
+class student{
     public String name;
     public String s_id;
     static String college = "STCET";
-
-    public student(String name, String s_id) {
+    public student(String name,String s_id){
         this.name = name;
         this.s_id = s_id;
     }
-
-    public static void show() {
+    public static void show(){
         System.out.println("In Static method show");
         System.out.println("College : " + college);
     }
-
-    public void display() {
+    public void display(){
         System.out.println("In non-static method display");
         System.out.println("Name: " + this.name);
         System.out.println("Student id: " + this.s_id);
     }
 }
-
 class Main1 {
     public static void main(String[] args) {
-        student s1 = new student("memeber", "ad3");
+        student s1 = new student("Naruto", "ad11");
         student.show();
         s1.display();
 
