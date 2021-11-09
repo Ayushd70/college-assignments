0d36b968ea8b35a6a1a17816a2b6d151c36cdf4e
Ayush Dubey <ayushdubey70@gmail.com>
2021-11-09 19:08:21 +0530

    OOPs: assignemnt-7: static bloc called before main()
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ OOPs-Java/assignment-7/codes/staticBloc.java
@@ -0,0 +1,11 @@
+public class staticBloc {
+    static {
+        System.out.println("It's static block... ");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("It's Main block... ");
+    }
+
+
+}
