ebf6e927297329f6afb3b9dc3165304a0822fb0b
Ayush Dubey <ayushdubey70@gmail.com>
2021-12-08 18:44:01 +0530

    OS: assignment-4: program 2 on signals
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ OS-Lab/assignment-5/programs/p2.c
@@ -0,0 +1,17 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+void oh(int sig)
+{
+    printf("OH! - I got signal %d\n", sig);
+    signal(SIGINT, SIG_DFL); /*resets the signal to default*/
+}
+int main()
+{
+    signal(SIGINT, oh);
+    while (1)
+    {
+        printf("Hello World!\n");
+        sleep(1);
+    }
+}
