5ece9a1bf21bbda0b8990bedb89f6b312c160d9b
Ayush Dubey <ayushdubey70@gmail.com>
2022-07-24 20:54:21 +0530

    DBMS: aggregate function problem
    * not borning movies problem
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ DBMS/Aggregate Func/SQL/notBorMovies.sql
@@ -0,0 +1,30 @@
+Write an SQL query to report the movies with an odd-numbered ID and a description that is not "boring".
+Return the result table ordered by rating in descending order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input: 
+Cinema table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 1  | War        | great 3D    | 8.9    |
+| 2  | Science    | fiction     | 8.5    |
+| 3  | irish      | boring      | 6.2    |
+| 4  | Ice song   | Fantacy     | 8.6    |
+| 5  | House card | Interesting | 9.1    |
++----+------------+-------------+--------+
+Output: 
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 5  | House card | Interesting | 9.1    |
+| 1  | War        | great 3D    | 8.9    |
++----+------------+-------------+--------+
+Explanation: 
+We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
+
+Ans:
+select * from cinema where mod(id,2)=1 and  description not like 'boring' order by rating desc;
